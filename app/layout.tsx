29988ad3d080cde217a9cf35ffd250674b0b0f0d
Thomas Zhang <thomas@mier.cat>
2024-07-18 00:06:45 -0700

    feat: added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "styles/tailwind.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>PathLynx Careers</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
