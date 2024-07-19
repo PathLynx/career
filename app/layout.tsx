d40a175498a5f9b92b0e474e4c93533d21e2bf94
Thomas Zhang <thomas@mier.cat>
2024-07-19 05:09:52 -0700

    fix: favicon route

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={notoSansSC.className}>
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+        <link rel="apple-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <title>PathLynx Careers</title>
       </head>
       <body className="flex min-h-screen flex-col bg-gray-400">
